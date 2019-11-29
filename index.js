79b0daec2063a4aefe39a2cd86afc727316c7b09
노수장 <solitas@nosujang-ui-iMac.local>
2019-11-29 21:27:08 +0900

    2.6 express core: Middlewares

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ const handleListening = () => console.log(`Listening on: http://localhost:${PORT
 const handleHome = (req, res) =>  res.send("Hello from home")
 const handleProfile = (req, res) =>  res.send("You are on my profile2")
 
+const betweenHome = (req, res, next) => {
+    console.log("I'm between");
+    next();
+}
+
+app.use(betweenHome)
 
 app.get('/', handleHome)
+
 app.get("/profile", handleProfile)
+ 
 app.listen(PORT, handleListening)
