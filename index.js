28a220bc5f9baa03e8798af8162db218b1c546f3
노수장 <solitas@nosujang-ui-iMac.local>
2019-11-29 21:39:30 +0900

    2.7 Express Core: Middlewarers part 2

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express"
+import morgan from "morgan"
+import helmet from "helmet"
+import cookieParser from "cookie-parser"
+import bodyParser from "body-parser"
+
 
 const app = express()
 
@@ -8,12 +13,11 @@ const handleListening = () => console.log(`Listening on: http://localhost:${PORT
 const handleHome = (req, res) =>  res.send("Hello from home")
 const handleProfile = (req, res) =>  res.send("You are on my profile2")
 
-const betweenHome = (req, res, next) => {
-    console.log("I'm between");
-    next();
-}
-
-app.use(betweenHome)
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get('/', handleHome)
 
