eb8b601b24293876ef3ce6b76f960f3bd928e4c9
노수장 <solitas@nosujang-ui-iMac.local>
2019-11-29 21:58:30 +0900

    2.8 Express Core: Routing

--- app.js
+++ app.js
@@ -3,13 +3,10 @@ import morgan from "morgan"
 import helmet from "helmet"
 import cookieParser from "cookie-parser"
 import bodyParser from "body-parser"
-
+import { userRouter } from "./router"
 
 const app = express()
 
-const PORT = 4000;
-
-const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`)
 const handleHome = (req, res) =>  res.send("Hello from home")
 const handleProfile = (req, res) =>  res.send("You are on my profile2")
 
@@ -22,5 +19,7 @@ app.use(morgan("dev"));
 app.get('/', handleHome)
 
 app.get("/profile", handleProfile)
- 
-app.listen(PORT, handleListening)
+
+app.use("/user", userRouter)
+
+export default app;
