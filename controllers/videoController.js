5d49d78efb370d39aae6854db040619b1f585c10
노수장 <solitas@nosujang-ui-iMac.local>
2019-12-25 11:24:33 +0900

    #2.21 Home Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,7 @@
-
-export const home = (req, res) => res.render("home", { pageTitle : "Home"});
+import { videos } from "../db";
+export const home = (req, res) => {
+  res.render("home", { pageTitle: "Home", videos });
+};
 export const search = (req, res) => {
     const { 
         query: { term: searchingBy}
@@ -7,10 +9,6 @@ export const search = (req, res) => {
     console.log(searchingBy);
 
     res.render("search", { pageTitle : "Search", searchingBy });
-}
-export const videos = (req, res) => {
-   
-    res.render("videos", { pageTitle : "Videos"});
 }
 export const upload = (req, res) => res.render("upload", { pageTitle : "Upload" });
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle : "Video Detail" });
