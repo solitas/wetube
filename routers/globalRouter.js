e8402083897d25eba2c28cfa648faec5b7479928
노수장 <solitas@nosujang-ui-iMac.local>
2019-12-26 20:36:03 +0900

    #2.24 Log In and User Profile Controller

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -3,18 +3,22 @@ import routes from "../routes"
 import { home, search } from "../controllers/videoController";
 import {
     getJoin,
-    login,
+    getLogin,
     logout,
-    postJoin
+    postJoin,
+    postLogin
   } from "../controllers/userController";
 const globalRouter = express.Router();
 
 globalRouter.get(routes.join, getJoin);
 globalRouter.post(routes.join, postJoin);
 
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
 globalRouter.get(routes.home, home);
 
-globalRouter.get(routes.login, login);
+
 globalRouter.get(routes.logout, logout);
 globalRouter.get(routes.search, search);
 
