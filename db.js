fd7a8f22b8d9020a965e1cb3c544ff61086c79bc
노수장 <solitas@nosujang-ui-iMac.local>
2019-12-28 09:03:13 +0900

    #3.2 Configuring Dot Env

--- db.js
+++ db.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose"
+import dotenv from "dotenv"
 
-mongoose.connect("mongodb://localhost:27017/we-tube", {
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO_URL, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
