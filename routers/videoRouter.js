da89e09216099ee756719d7f998fb00c9c442dfc
노수장 <solitas@nosujang-ui-iMac.local>
2019-12-28 14:49:42 +0900

    #3.10 Deleting a Video

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -17,6 +17,6 @@ videoRouter.get(routes.videoDetail(), videoDetail);
 
 videoRouter.get(routes.editVideo(), getEditVideo);
 videoRouter.get(routes.editVideo(), postEditVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
