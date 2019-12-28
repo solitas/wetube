5e34aaa19754593b5dc21dd07eb75bb414da25ea
노수장 <solitas@nosujang-ui-iMac.local>
2019-12-28 09:13:45 +0900

    #3.3 Video Model

--- init.js
+++ init.js
@@ -3,6 +3,9 @@ import app from "./app";
 import dotenv from "dotenv";
 dotenv.config();
 
+import "./models/Video";
+
+
 const PORT = process.env.PORT;
 
 const handleListening = () => console.log(`✅ Listening on: https://localhost:${PORT}`);
