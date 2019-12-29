03130ce9b952a0854846d08ee155dbbda15ac345
노수장 <solitas@nosujang-ui-iMac.local>
2019-12-29 16:33:17 +0900

    #4.3 ES6 with Webpack

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-import express from "express"
-import morgan from "morgan"
-import helmet from "helmet"
-import cookieParser from "cookie-parser"
-import bodyParser from "body-parser"
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
-import globalRouter from "./routers/globalRouter"
-import userRouter from "./routers/userRouter"
-import videoRouter from "./routers/videoRouter"
-import routes from "./routes"
-import { localsMiddleware } from "./middlewares"
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
-
 
 // middlewares
 app.use(helmet());
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 app.use(localsMiddleware);
 // routers
-app.use(routes.home,    globalRouter);
-app.use(routes.users,   userRouter);
-app.use(routes.videos,  videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
